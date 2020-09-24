728a28538986c288ad0f126c420646db3f074c98
Oliver Ford <dev@ojford.com>
2020-09-24 13:22:01 +0100

    Add private_key output example
    Queried in #1.

--- examples/asymmetric_key/outputs.tf
+++ examples/asymmetric_key/outputs.tf
@@ -2,3 +2,9 @@ output "wg_public_key" {
   description = "Example's public WireGuard key"
   value       = wireguard_asymmetric_key.example.public_key
 }
+
+output "wg_private_key" {
+  description = "Example's private WireGuard key"
+  value       = wireguard_asymmetric_key.example.private_key
+  sensitive   = true
+}
